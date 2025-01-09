610794767693637cccb0e83f2fe023f4977e6a31
dinesh69012 <168328706+dinesh69012@users.noreply.github.com>
2025-01-09 13:25:04 +0545

    javascript done

--- AWE_DINESH_BAHADUR_AYER_77356697/resources/js/app.js
+++ AWE_DINESH_BAHADUR_AYER_77356697/resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import.meta.glob([ '../images/**', ]);
+import './theme';
+
+window.confirmDelete = function(id, name){
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You wont be able to restore it!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById(name + '_' + id).submit();
+            }
+        })
+}
+
+window.cannot = function(message){
+    Swal.fire(
+        'Error',
+        message,
+        'error'
+    )
+}
